16e92b92adcd112f02b648623875ebea700639d6
diego-tavares <102318991+diego-tavares@users.noreply.github.com>
2022-07-26 11:16:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Open the Modal
 function openModal() {
   document.getElementById("myModal").style.display = "block";
@@ -38,27 +37,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-
-// Comments
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
